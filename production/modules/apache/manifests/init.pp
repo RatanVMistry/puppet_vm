00327f7e00b824a1f2e94a2056c8d75e903621be
root <root@learning.puppetlabs.vm>
2016-07-28 17:36:10 -0700

    second

--- production/modules/apache/manifests/init.pp
+++ production/modules/apache/manifests/init.pp
@@ -16,22 +16,26 @@ class apache {
                 content => "<h1> hello </h1>",
                  require => Package['apache2'],
       }
-      file { '/home/vagrant/test.txt':
-                content => template('apache/temp.txt.erb'),
-      }
-      @@file { "/etc/nginx/conf.d/${::hostname}.conf" :
-                path => "/etc/nginx/conf.d/${::hostname}.conf",
-                content => "${::ipaddress_eth1}\n",
-                tag => 'web_server',
-                ensure => file,
-    }
+      # file { '/home/vagrant/test.txt':
+      #         content => template('apache/temp.txt.erb'),
+      #}
+      #@@file { "/etc/nginx/conf.d/${::hostname}.conf" :
+      #         path => "/etc/nginx/conf.d/${::hostname}.conf",
+      #         content => "${::ipaddress_eth1}\n",
+      #         tag => 'web_server',
+      #         ensure => file,
+      # }
     @@file_fragment { "uniqe_name_${::fqdn}":
-      content => "${::ipaddress_eth1};\n",
+      content => "server  ${::ipaddress_eth1};\n",
       tag     => 'web',
     }
     File_fragment <<| tag == 'web' |>>
-    file_concat { '/tmp/file.txt':
-      tag => 'web', # Mandatory
+    file_concat { '/tmp/test.txt':
+     tag     => 'web', # Mandatory
+     replace => true
     }
+    # file { '/home/vagrant/test.txt':
+    #                  content => template('apache/testi.txt.erb'),
+    #   }
     #motd::register{ 'Apache': }
 }
