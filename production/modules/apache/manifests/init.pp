06b5e46bfec175ad34e86fb2edc0c876d9f625e0
ratan <ratan_30@yahoo.co.in>
2016-07-28 21:18:49 -0700

    second

--- production/modules/apache/manifests/init.pp
+++ production/modules/apache/manifests/init.pp
@@ -12,10 +12,26 @@ class apache {
                   require => Package['apache2'],
               }
 
-      file { '/var/www/html/index.html':
-                content => "<h1> hello </h1>",
-                 require => Package['apache2'],
+      package { 'php5':
+          require  => Exec['apt-update'],
+          ensure => installed,
       }
+
+      file { '/etc/apache2/mods-enabled/dir.conf':
+                    ensure    => file,
+                    content  => template('apache/dir.conf.erb'),
+                    require => Package['apache2'],
+          }
+      file { '/var/www/html/index.php':
+          ensure    => file,
+          content => template('apache/index.php.erb'),
+          require => Package['apache2'],
+      }
+
+      # file { '/var/www/html/index.html':
+      #         content => "<h1> hello </h1>",
+      #          require => Package['apache2'],
+      #}
       # file { '/home/vagrant/test.txt':
       #         content => template('apache/temp.txt.erb'),
       #}
@@ -25,15 +41,31 @@ class apache {
       #         tag => 'web_server',
       #         ensure => file,
       # }
-    @@file_fragment { "uniqe_name_${::fqdn}":
-      content => "server  ${::ipaddress_eth1};\n",
-      tag     => 'web',
-    }
-    File_fragment <<| tag == 'web' |>>
-    file_concat { '/tmp/test.txt':
-     tag     => 'web', # Mandatory
-     replace => true
-    }
+      # @@file_fragment { "uniqe_name_f${::fqdn}":
+      #        content => "upstream cluster { \n",
+      #        tag    => 'web',
+      #}
+      @@file_fragment { "uniqe_name_${::fqdn}":
+              content => "server  ${::ipaddress_eth1};\n",
+              tag     => 'web',
+              order   => 2,
+      }
+      #@@file_fragment { "uniqe_name_d${::fqdn}":
+      #                  content => " }",
+      #                   tag    => 'web',
+      #          }
+      #@@file_down { 'down' :
+      # content => "}",
+      # tag     => 'web',
+      # }
+      #File_up <<| tag == 'web' |>>
+      # File_fragmentt <<| tag == 'web' |>>
+      #File_fragment <<| tag == 'web' |>>
+    # File_down <<| tag == 'web' |>>
+    #file_concat { '/tmp/test.txt':
+    #tag     => 'web', # Mandatory
+    #replace => true
+    #}
     # file { '/home/vagrant/test.txt':
     #                  content => template('apache/testi.txt.erb'),
     #   }
--- production/modules/nginx/manifests/init.pp
+++ production/modules/nginx/manifests/init.pp
@@ -18,8 +18,30 @@ class nginx{
                       require => Package['nginx'],
                   }
                   # File <<| tag == 'web_server' |>>
+                  #@@file_fregment { "uniqe_name_lb${::fqdn}":
+                  #content => "upstream cluster { \n",
+                  #order   => 0,
+                  #tag     => 'lb'
+                  #}
+
+                  @@file_fragment { "uniqe_name_l${::fqdn}":
+                             content => "upstream cluster { \n",
+                              tag    => 'web',
+                               order => 0,
+                  }
+
+                  @@file_fragment { "uniqe_name_lo${::fqdn}":
+                          content   => "}",
+                          tag     => 'web',
+                          order => 10,
+                        }
+
           File_fragment <<| tag == 'web' |>>
+
            file_concat { '/etc/nginx/conf.d/up.conf':
-                     tag => 'web', # Mandatory
+                     tag   => 'web', # Mandatory
             }
+            #file_concat { '/etc/nginx/conf.d/up.conf':
+            #  tag   => 'lb',
+            #}
 }
